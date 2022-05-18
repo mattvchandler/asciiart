3db0b9f9b16a31b989e4559d864df4a957b8662c
mattvchandler <tardarsauce@gmail.com>
2022-05-17 16:59:19 -0800

    add more features to binio - r/w w/ iter/pointers

--- codecs/binio.hpp
+++ codecs/binio.hpp
@@ -9,6 +9,38 @@
 #include <cstdint>
 #include <type_traits>
 
+template<typename T> concept Byte_input_iter =
+#ifdef _cpp_lib_concepts
+std::input_iterator<T> &&
+#else
+// poor man's input_iterator
+requires(T begin, T end)
+{
+    begin == end;
+    ++begin;
+    *begin = std::declval<decltype(*begin)>();
+    *begin++ = std::declval<decltype(*begin)>();
+    requires std::is_same_v<decltype(begin == end), bool>;
+    requires std::is_same_v<decltype(++begin), T&>;
+} &&
+#endif
+requires { requires sizeof(*std::declval<T>()) == 1; };
+
+template<typename T> concept Byte_output_iter =
+#ifdef _cpp_lib_concepts
+std::output_iterator<T>;
+#else
+// poor man's output_iterator
+requires(T begin)
+{
+    *begin = std::declval<decltype(*begin)>();
+    ++begin;
+    begin++;
+    *begin++ = std::declval<decltype(*begin)>();
+    requires std::is_same_v<decltype(++begin), T&>;
+};
+#endif
+
 // mixed endian systems apparently do exist, so do a static_assert to make sure we're one or the other
 static_assert(std::endian::native == std::endian::big || std::endian::native == std::endian::little);
 
@@ -68,10 +100,58 @@ void readb(std::istream & i, T & t, std::endian endian = std::endian::little)
         t = bswap(t);
 }
 
+template <typename T> requires (!std::is_enum_v<T>)
+T readb(std::istream & i, std::endian endian = std::endian::little)
+{
+    T t{0};
+    readb(i, t, endian);
+    return t;
+}
+
+template <typename T, Byte_input_iter InputIter> requires (!std::is_enum_v<T>)
+void readb(InputIter & begin, InputIter end, T & t, std::endian endian = std::endian::little)
+{
+    auto & buf = reinterpret_cast<std::byte(&)[sizeof(T)]>(t);
+    for(auto && i: buf)
+    {
+        if(begin == end)
+            throw std::runtime_error{"Unexpected end of input"};
+        i = static_cast<std::byte>(*begin++);
+    }
+    if(std::endian::native != endian)
+        t = bswap(t);
+}
+
+template <typename T, Byte_input_iter InputIter> requires (!std::is_enum_v<T>)
+T readb(InputIter & begin, InputIter end, std::endian endian = std::endian::little)
+{
+    T t{0};
+    readb(begin, end, t, endian);
+    return t;
+}
+
 template<typename E> requires std::is_enum_v<E>
 void readb(std::istream & i, E & e, std::endian endian = std::endian::little)
 {
     readb<std::underlying_type_t<E>>(i, reinterpret_cast<std::underlying_type_t<E>&>(e), endian);
+}
+
+template<typename E> requires std::is_enum_v<E>
+E readb(std::istream & i, std::endian endian = std::endian::little)
+{
+    return static_cast<E>(readb<std::underlying_type_t<E>>(i, endian));
+}
+
+template<typename E, Byte_input_iter InputIter> requires std::is_enum_v<E>
+void readb(InputIter & begin, InputIter end, E & e, std::endian endian = std::endian::little)
+{
+    readb<std::underlying_type_t<E>>(begin, end, reinterpret_cast<std::underlying_type<E>&>(e), endian);
+}
+
+template<typename E, Byte_input_iter InputIter> requires std::is_enum_v<E>
+E readb(InputIter & begin, InputIter end, std::endian endian = std::endian::little)
+{
+    return static_cast<E>(readb<std::underlying_type_t<E>>(begin, end, endian));
 }
 
 inline std::string readstr(std::istream & i, std::size_t len)
@@ -79,6 +159,19 @@ inline std::string readstr(std::istream & i, std::size_t len)
     std::string s(len, ' ');
     i.read(std::data(s), len);
 
+    return s;
+}
+
+template <Byte_input_iter InputIter>
+std::string readstr(InputIter & begin, InputIter end, std::size_t len)
+{
+    std::string s(len, '\0');
+    for(std::size_t i = 0; i < len; ++i)
+    {
+        if(begin == end)
+            throw std::runtime_error{"Unexpected end of input"};
+        s[i] = static_cast<char>(*begin++);
+    }
     return s;
 }
 
@@ -91,8 +184,24 @@ void writeb(std::ostream & o, T t, std::endian endian = std::endian::little)
     o.write(buf, sizeof(buf));
 }
 
+template <typename T, Byte_output_iter OutputIter> requires(!std::is_enum_v<T>)
+void writeb(OutputIter & o, T t, std::endian endian = std::endian::little)
+{
+    if(std::endian::native != endian)
+        t = bswap(t);
+    auto & buf = reinterpret_cast<char(&)[sizeof(T)]>(t);
+    for(auto && b: buf)
+        *o++ = b;
+}
+
 template <typename E> requires(std::is_enum_v<E>)
 void writeb(std::ostream & o, E e, std::endian endian = std::endian::little)
+{
+    writeb(o, static_cast<std::underlying_type_t<E>>(e), endian);
+}
+
+template <typename E, Byte_output_iter OutputIter> requires(std::is_enum_v<E>)
+void writeb(OutputIter & o, E e, std::endian endian = std::endian::little)
 {
     writeb(o, static_cast<std::underlying_type_t<E>>(e), endian);
 }
@@ -100,6 +209,13 @@ void writeb(std::ostream & o, E e, std::endian endian = std::endian::little)
 inline void writestr(std::ostream & o, const std::string_view & s)
 {
     o.write(std::data(s), std::size(s));
+}
+
+template <Byte_output_iter OutputIter>
+inline void writestr(OutputIter & o, const std::string_view & s)
+{
+    for(auto && b: s)
+        *o++ = b;
 }
 
 #endif // BINIO_HPP
