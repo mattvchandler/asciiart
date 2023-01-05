01c1ce338ef3f24a95315cc7b34786aed3d7183c
mattvchandler <tardarsauce@gmail.com>
2023-01-05 09:55:11 -0900

    fix jpeg detection

--- codecs/jpeg.hpp
+++ codecs/jpeg.hpp
@@ -6,13 +6,15 @@
 inline bool is_jpeg(const Image::Header & header)
 {
     const std::array<unsigned char, 4>  jpeg_header1   = {0xFF, 0xD8, 0xFF, 0xDB};
-    const std::array<unsigned char, 12> jpeg_header2   = {0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46, 0x00, 0x01};
+    const std::array<unsigned char, 4>  jpeg_header2_1 = {0xFF, 0xD8, 0xFF, 0xE0};
+    const std::array<unsigned char, 5>  jpeg_header2_2 = {0x4A, 0x46, 0x49, 0x46, 0x00}; // there are 2 "don't care" bytes between these
     const std::array<unsigned char, 4>  jpeg_header3   = {0xFF, 0xD8, 0xFF, 0xEE};
     const std::array<unsigned char, 4>  jpeg_header4_1 = {0xFF, 0xD8, 0xFF, 0xE1};
     const std::array<unsigned char, 6>  jpeg_header4_2 = {0x45, 0x78, 0x69, 0x66, 0x00, 0x00}; // there are 2 "don't care" bytes between these
 
     return std::equal(std::begin(jpeg_header1),   std::end(jpeg_header1),   std::begin(header), Image::header_cmp)
-       ||  std::equal(std::begin(jpeg_header2),   std::end(jpeg_header2),   std::begin(header), Image::header_cmp)
+       ||  (std::equal(std::begin(jpeg_header2_1),   std::end(jpeg_header2_1),   std::begin(header), Image::header_cmp)
+            && std::equal(std::begin(jpeg_header2_2), std::end(jpeg_header2_2), std::begin(header) + std::size(jpeg_header2_1) + 2, Image::header_cmp))
        ||  std::equal(std::begin(jpeg_header3),   std::end(jpeg_header3),   std::begin(header), Image::header_cmp)
        || (std::equal(std::begin(jpeg_header4_1), std::end(jpeg_header4_1), std::begin(header), Image::header_cmp)
            && std::equal(std::begin(jpeg_header4_2), std::end(jpeg_header4_2), std::begin(header) + std::size(jpeg_header4_1) + 2, Image::header_cmp));
